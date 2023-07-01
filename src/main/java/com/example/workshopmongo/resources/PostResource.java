0b02bfecffbc3a98ac833cddaefe4a03b7e6053d
Ryan <ryan.rosa.profissional@gmail.com>
2023-07-01 06:56:25 -0300

    Realizando uma busca em todos os campos de texto procurando por determinado parametro

--- src/main/java/com/example/workshopmongo/resources/PostResource.java
+++ src/main/java/com/example/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,18 @@ public class PostResource {
 	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String title) {
 		List<Post> posts = postService.findByTitle(URL.decodeParam(title));
+		return ResponseEntity.ok().body(posts);
+	}
+	
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> posts = postService.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(posts);
 	}
 }
